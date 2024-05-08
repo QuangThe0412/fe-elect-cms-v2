97e94581c453c93a40c78c383a49e4fd11b1b765
BoyCoDon <thenq@dtp-education.com>
2024-05-08 16:20:53 +0700

    work on product

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,9 @@ import { Avatar } from 'primereact/avatar';
 const Header = () => {
     return (
         <div className='wrapper-header'>
-            <div>
-                <h1>Logo</h1>
-            </div>
-            <div>
-                <h1>Store</h1>
-            </div>
-            <div>
-                <Avatar icon="pi pi-user" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} shape="circle" />
-            </div>
+            <h1>Logo</h1>
+            <h1>Store</h1>
+            <Avatar icon="pi pi-user" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }}/>
         </div>
     );
 };
