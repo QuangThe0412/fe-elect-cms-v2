f136c9aa654dbb133b5030404b8ab30fd799be7c
BoyCoDon <thenq@dtp-education.com>
2024-05-15 16:47:30 +0700

    loading state add

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -11,11 +11,12 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
   const [selectedProduct, setSelectedProduct] = useState<Product[] | null>(null);
   const [rowClick, setRowClick] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     ProductService.getProducts().then(data => {
-      console.log(data)
       setProducts(data.data)
+      setLoading(false);
     });
   }, []);
 
@@ -47,22 +48,23 @@ export default function Products() {
         paginator rows={13} rowsPerPageOptions={[5, 10, 25, 50]}
         stripedRows sortMode="multiple" removableSort
         tableStyle={{ width: '100%' }}
+        loading={loading}
         selectionMode="single" selection={selectedProduct}
         onSelectionChange={(e: any) => setSelectedProduct(e.value)} dataKey="IDMon"
       >
-        <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
-        <Column field="IDMon" header="Id" style={{ width: '7%' }}></Column>
-        <Column field="IDLoaiMon" header="Loại" style={{ width: '7%' }}></Column>
+        <Column selectionMode="single" headerStyle={{ width: '3rem' }}></Column>
+        <Column field="IDMon" header="Id" ></Column>
+        <Column field="IDLoaiMon" header="Loại" ></Column>
         <Column field="TenMon" header="Tên" style={{ width: '30%' }}></Column>
-        <Column field="Image" header="Hình ảnh" style={{ width: '7%' }}></Column>
-        <Column field="DVTMon" header="ĐVT" style={{ width: '7%' }}></Column>
-        <Column field="DonGiaVon" header="Giá vốn" body={bodyDonGiaVon} sortable style={{ width: '7%' }}></Column>
-        <Column field="DonGiaBanLe" header="Giá lẻ" body={bodyDonGiaLe} sortable style={{ width: '7%' }}></Column>
-        <Column field="DonGiaBanSi" header="Giá sỉ" body={bodyDonGiaBanSi} sortable style={{ width: '7%' }}></Column>
-        <Column field="SoLuongTonKho" header="Tồn kho" body={bodyTonKho} sortable style={{ width: '7%' }}></Column>
-        <Column field="ThoiGianBH" header="Bảo hành" sortable style={{ width: '7%' }}></Column>
-        <Column field="Deleted" header="Trạng thái" body={statusBodyTemplate} style={{ width: '7%' }}></Column>
-        <Column field="GhiChu" header="Ghi chú" style={{ width: '7%' }}></Column>
+        <Column field="Image" header="Hình ảnh" ></Column>
+        <Column field="DVTMon" header="ĐVT" ></Column>
+        <Column field="DonGiaVon" header="Giá vốn" body={bodyDonGiaVon} sortable ></Column>
+        <Column field="DonGiaBanLe" header="Giá lẻ" body={bodyDonGiaLe} sortable ></Column>
+        <Column field="DonGiaBanSi" header="Giá sỉ" body={bodyDonGiaBanSi} sortable ></Column>
+        <Column field="SoLuongTonKho" header="Tồn kho" body={bodyTonKho} sortable ></Column>
+        <Column field="ThoiGianBH" header="Bảo hành" sortable ></Column>
+        <Column field="Deleted" header="Trạng thái" body={statusBodyTemplate} ></Column>
+        <Column field="GhiChu" header="Ghi chú" ></Column>
       </DataTable>
     </div>
   );
